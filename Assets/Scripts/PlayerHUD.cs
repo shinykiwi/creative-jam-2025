eb7b6c45291b2994006a32291a104d2735a2de5c
Kira <94204499+shinykiwi@users.noreply.github.com>
2025-09-20 12:08:00 -0400

    test cube for zaid

--- Assets/Scripts/PlayerHUD.cs
+++ Assets/Scripts/PlayerHUD.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 public class PlayerHUD : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI itemCountText;
-    
+    [SerializeField] TextMeshProUGUI helperTextObject;
+    private string helperText;
     private float itemCount;
+
+    public string HelperText
+    {
+        get => helperText;
+        set 
+        {
+            helperText = value;
+            SetHelperText(helperText);
+        }
+    }
     public float ItemCount
     {
         get => itemCount;
@@ -20,5 +31,10 @@ public class PlayerHUD : MonoBehaviour
     private void SetItemCountText(float count)
     {
         itemCountText.text = "x" + count;
+    }
+
+    private void SetHelperText(string text)
+    {
+        helperTextObject.text = text;
     }
 }
---
+++ Assets/Scripts/PlayerRaycast.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerRaycast : MonoBehaviour
+{
+    public GameObject LastItem { get; set; } = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Sending a raycast from the mouse position (which is already locked to middle of screen)
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        // If something was hit
+        if (Physics.Raycast(ray, out RaycastHit hit, 8f))
+        {
+            GameObject hitObject = hit.collider.gameObject;
+
+            if (hitObject.GetComponent<Item>() is { } item)
+            {
+                LastItem = hitObject;
+                item.OnLookAt();
+            }
+        }
+        else
+        {
+            LastItem = null;
+        }
+    }
+}
