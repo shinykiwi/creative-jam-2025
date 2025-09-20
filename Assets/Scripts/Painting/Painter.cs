690288964176dc10467bfa2f73bb87c5dc04734f
Zaid Minhas <61346884+ZaidMinhas@users.noreply.github.com>
2025-09-20 13:05:10 -0400

    ProBuilder

--- Assets/Scripts/Painting/Painter.cs
+++ Assets/Scripts/Painting/Painter.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Painter : MonoBehaviour
@@ -5,18 +6,34 @@ public class Painter : MonoBehaviour
     public Camera cam;
     public float brushSize = 10f;
 
+    public void Awake()
+    {
+        if (cam == null)
+            cam = Camera.main;
+    }
+
     void Update()
     {
         if (Input.GetMouseButton(0))
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out RaycastHit hit))
-            {
-                RealityPaintMask rpm = hit.collider.GetComponent<RealityPaintMask>();
+            Paint(true);
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            Paint(false);
+        }
+    }
+
+    private void Paint(bool add)
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+            RealityPaintMask rpm = hit.collider.GetComponent<RealityPaintMask>();
                 
-                if (rpm)
-                    rpm.PaintAtUV(hit.textureCoord, brushSize);
-            }
+            if (rpm)
+                rpm.PaintAtUV(hit.textureCoord, brushSize, add);
         }
     }
 }
--- Assets/Scripts/Painting/RealityPaintMask.cs
+++ Assets/Scripts/Painting/RealityPaintMask.cs
@@ -3,30 +3,35 @@ using UnityEngine;
     public class RealityPaintMask : MonoBehaviour
     {
         private static readonly int RealityMask = Shader.PropertyToID("_RealityMask");
-        public Texture2D maskTex;
-        public Renderer rend;
+        private Texture2D maskTex;
+        private Renderer rend;
+        private MeshCollider meshCollider;
+        public bool isVisible;
         public int maskResolution = 512;
+        private int paintedPixels = 0;
+        private float PaintedPercent => 2*paintedPixels / (float)(maskResolution*maskResolution);
+
+        [Range(0f, 1f)] public float threshold;
+        
 
         void Start()
         {
             rend = GetComponent<Renderer>();
             maskTex = new Texture2D(maskResolution, maskResolution, TextureFormat.RGBA32, false);
+            meshCollider = GetComponent<MeshCollider>();
             Color[] cols = new Color[maskResolution * maskResolution];
-            for (int i = 0; i < cols.Length; i++) cols[i] = Color.black;
+            for (int i = 0; i < cols.Length; i++) cols[i] = isVisible? Color.white : Color.black;
             maskTex.SetPixels(cols);
             maskTex.Apply();
 
             rend.material.SetTexture(RealityMask, maskTex);
         }
 
-        // Called by player script
-        public void PaintAtUV(Vector2 uv, float brushSize)
+        public void PaintAtUV(Vector2 uv, float brushSize, bool add = true)
         {
             int x = (int)(uv.x * maskResolution);
             int y = (int)(uv.y * maskResolution);
             
-            Debug.Log(uv);
-
             for (int i = - (int)brushSize; i < brushSize; i++)
             {
                 for (int j = - (int)brushSize; j < brushSize; j++)
@@ -37,11 +42,54 @@ using UnityEngine;
                     {
                         float dist = Vector2.Distance(new Vector2(x, y), new Vector2(px, py));
                         if (dist < brushSize)
-                            maskTex.SetPixel(px, py, Color.white);
+                        {
+
+                            if (add)
+                            {
+                                if (maskTex.GetPixel((int)px, (int)py).r == 0f)
+                                {
+                                    paintedPixels++;
+                                    maskTex.SetPixel(px, py, Color.white);
+                                }    
+                            }
+                            else
+                            {
+                                if (Mathf.Approximately(maskTex.GetPixel((int)px, (int)py).r, 1f))
+                                {
+                                    paintedPixels--;
+                                    maskTex.SetPixel(px, py, Color.black);
+                                }    
+                            }
+                            
+                        }
+                            
                     }
                 }
             }
+            
 
             maskTex.Apply();
+
+            
+            if (PaintedPercent > threshold && !isVisible)
+            {
+                Solidify();
+            }
+            else if (PaintedPercent < threshold && isVisible)
+            {
+                Disappear();
+            }
+        }
+
+        public void Solidify()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Default");
+            isVisible = true;
+        }
+
+        public void Disappear()
+        {
+            gameObject.layer = LayerMask.NameToLayer("FakeLayer");
+            isVisible = false;
         }
     }
